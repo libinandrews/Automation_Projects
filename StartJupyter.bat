4022bee47a6bb4bed4338d67d78e73e31ed9661e
Libin Andrews <andrewslibin@gmail.com>
2026-02-01 14:06:16 +0530

    Updated for Jupyter Lab from Jupyter notebook

--- StartJupyter.bat
+++ StartJupyter.bat
@@ -1,3 +1,12 @@
 @echo off
+REM Change to the directory where this script is located
 cd /d "%~dp0"
-jupyter notebook
+set ROOT_DIR=%cd%
+echo Root working directory set to: %ROOT_DIR%
+
+REM Install Jupyter Lab if not already installed
+pip install jupyterlab
+
+REM Start Jupyter Lab with the root directory
+jupyter lab --notebook-dir="%ROOT_DIR%"
+pause
